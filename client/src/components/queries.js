3cd26cab2ca39afd0b0c3fed0c6873f65b4206aa
raied Abdelgani <raiedabdelgani@gmail.com>
2023-03-25 14:44:28 -0400

    added mutations and queries

---
+++ client/src/components/queries.js
@@ -0,0 +1,20 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
